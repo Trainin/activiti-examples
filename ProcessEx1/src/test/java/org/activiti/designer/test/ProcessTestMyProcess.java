d7f3e4119f4cb44f25ae59279ddc4d3d027006ee
Trainin <flei@hotmail.it>
2015-05-07 15:04:12 +0200

    First Commit

---
+++ ProcessEx1/src/test/java/org/activiti/designer/test/ProcessTestMyProcess.java
@@ -0,0 +1,41 @@
+package org.activiti.designer.test;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.FileInputStream;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProcessTestMyProcess {
+
+	private String filename = "C:\\Users\\Luca\\workspace\\ProcessEx1\\MyProcess.bpmn";
+
+	@Rule
+	public ActivitiRule activitiRule = new ActivitiRule();
+
+	@Test
+	public void startProcess() throws Exception {
+		RepositoryService repositoryService = activitiRule.getRepositoryService();
+		
+		//deploys process definition
+		repositoryService.createDeployment().addInputStream("myProcess.bpmn20.xml",
+				new FileInputStream(filename)).deploy();
+		
+		RuntimeService runtimeService = activitiRule.getRuntimeService();
+		Map<String, Object> variableMap = new HashMap<String, Object>();
+		variableMap.put("name", "Activiti");
+		
+		//Starts a new process instance
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("myProcess", variableMap);
+		assertNotNull(processInstance.getId());
+		System.out.println("id " + processInstance.getId() + " "
+				+ processInstance.getProcessDefinitionId());
+	}
+}
